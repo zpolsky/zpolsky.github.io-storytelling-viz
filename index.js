077f4f3bccd355187e465e4ca4fe700fec37466e
Zach Polsky <zacharypolsky@wustl.edu>
2018-03-23 19:56:55 -0500

    Working Pie Chart
    Fixed pie chart

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let dataByYear = {};
+const groupYears = [-1, -2, -3, -4, -5, -6, -7];
+const extraButtons = groupYears.length;
 
 function rgb(r, g, b) {
   return `rgb(${r}, ${g}, ${b})`;
@@ -11,12 +13,10 @@ function getStateName(location) {
 function getAllYears(data) {
   // Create array to store all years for buttons
   const allYears = [];
-  const yearSet = new Set();
   data.forEach(d => {
     const date = (new Date(d.Date)).getFullYear();
-    if (!yearSet.has(date)) {
+    if (!allYears.includes(date)) {
       allYears.push(date);
-      yearSet.add(date);
     }
   });
   allYears.reverse();
@@ -32,8 +32,22 @@ function getAllYears(data) {
     })
     dataByYear[year] = yearData;
   });
-  allYears.push(-1); // used to display all data
+  allYears.push(...groupYears); // used to display all data and decades
   return allYears;
+}
+
+function checkDecade(key, year) {
+  const decade = (year >= 2000) ? year - 2000 : year - 1900;
+  switch (key) {
+    case -1: return true;
+    case -2: return decade >= 60 && decade < 70;
+    case -3: return decade >= 70 && decade < 80;
+    case -4: return decade >= 80 && decade < 90;
+    case -5: return decade >= 90 && decade <= 99;
+    case -6: return decade >= 0 && decade < 10;
+    case -7: return decade >= 10 && decade < 20;
+    default: return false;
+  }
 }
 
 // Adapted from http://bl.ocks.org/michellechandra/0b2ce4923dc9b5809922
@@ -62,26 +76,36 @@ function createMap(year, allYears) {
   let summaryBox = d3.select('body')
         .append('div')
         .attr('class', 'summaryBox')
-        .style('opacity', 1);
+        .style('opacity', 0)
+        .style('left', '1000px')
+        .style('top', '200px');
+
+  let pieDiv = d3.select('body')
+        .append('div')
+        .attr('class', 'pieDiv')
+        .style('opacity', 1)
+        .style('left', '1000px')
+        .style('top', '200px');
 
   d3.json('./data/us-states.json', json => {
-
-    if (year === -1) {
-      for (let i = 0; i < allYears.length - 1; i++) {
+    if (groupYears.includes(year)) {
+      for (let i = 0; i < allYears.length - extraButtons; i++) {
         const currentYear = allYears[i];
-        dataByYear[currentYear].forEach(d => {
-          for (let j = 0; j < json.features.length; ++j) {
-            const jsonState = json.features[j].properties.name;
-            const stateName = getStateName(d.Location);
-            if (stateName === jsonState) {
-              if (!json.features[j].properties.shootings) {
-                json.features[j].properties.shootings = [d];
-              } else {
-                json.features[j].properties.shootings.push(d);
+        if (checkDecade(year, currentYear)) {
+          dataByYear[currentYear].forEach(d => {
+            for (let j = 0; j < json.features.length; ++j) {
+              const jsonState = json.features[j].properties.name;
+              const stateName = getStateName(d.Location);
+              if (stateName === jsonState) {
+                if (!json.features[j].properties.shootings) {
+                  json.features[j].properties.shootings = [d];
+                } else {
+                  json.features[j].properties.shootings.push(d);
+                }
               }
             }
-          }
-        });
+          });
+        }
       };
     } else {
       dataByYear[year].forEach(d => {
@@ -117,7 +141,6 @@ function createMap(year, allYears) {
       .on('click', d => {
         const shootings = d.properties.shootings;
         if (shootings) {
-
           let stateVictims = 0;
           let stateFatalities = 0;
           let statePolice = 0;
@@ -129,6 +152,8 @@ function createMap(year, allYears) {
             statePolice += shooting.Policeman_Killed;
             stateInjured += shooting.Injured;
           });
+
+          pieDiv.style('opacity', 0);
 
           summaryBox.html(() => {
             let text = `
@@ -143,31 +168,29 @@ function createMap(year, allYears) {
             <span><b>Shootings</b>:</span>
             <ul>
             `;
-            console.log('shootings = ', shootings);
             shootings.forEach(shooting => {
               text += `<li><em>${shooting.Title} (${(new Date(shooting.Date)).getFullYear()})</em></li>`
             });
             text += '</ul>';
             return text;
           })
-          .style('left', '1000px')
-          .style('top', '200px');
+          .style('opacity', 1);
         } else {
-          summaryBox.html('')
-          .style('left', '1000px')
-          .style('top', '200px');
+          summaryBox.html('').style('opactiy', 0);
         }
-      })
+      });
 
     svg.selectAll('circle')
       .data(() => {
-        if (year === -1) {
+        if (groupYears.includes(year)) {
           const allData = [];
-          for (let i = 0; i < allYears.length - 1; i++) {
+          for (let i = 0; i < allYears.length - extraButtons; i++) {
             const currentYear = allYears[i];
-            dataByYear[currentYear].forEach(d => {
-              allData.push(d);
-            });
+            if (checkDecade(year, currentYear)) {
+              dataByYear[currentYear].forEach(d => {
+                allData.push(d);
+              });
+            }
           };
           return allData;
         } else {
@@ -205,6 +228,7 @@ function createMap(year, allYears) {
           `
           <h5><b>${d.Title} (${(new Date(d.Date)).getFullYear()})</b></h5>
           <p><b>Location</b>: ${d.Location}</p>
+          <p><b>Date</b>: ${d.Date}</p>
           <p><b>Summary</b>: ${d.Summary}</p>
           <span><b>Total Victims</b>: ${d.Total_Victims}</span>
           <ul>
@@ -218,18 +242,169 @@ function createMap(year, allYears) {
             <li>Age: ${d.Age}</li>
             <li>Race: ${d.Race}</li>
             <li>Mental Health Issues: ${d.Mental_Health_Issues}</li>
+            <li>Employed: ${d.Employed}</li>
           </ul>
           `
         )
         .style('left', '1000px')
-        .style('top', '200px');
-      })
+        .style('top', '200px')
+        .style('opacity', 1);
+        pieDiv.style('opacity', 0);
+      });
+      createPieChart(year, pieDiv, allYears);
   });
 }
 
-function loadYear(year, allYears, maxYear) {
+function createPieChart(year, pieDiv, allYears) {
+  // Adapted from https://bl.ocks.org/mbostock/3887193
+  const pieWidth = 500;
+  const pieHeight = 500;
+  const radius = Math.min(pieWidth, pieHeight)/2;
+
+  pieDiv.append('h3')
+    .html(`Mental Health Summary: ${getYearText(year)}`)
+    .style('position', 'absolute')
+    .style('top', '50px')
+    .style('left', '-50px')
+
+  let pieSVG = pieDiv.append('svg')
+    .attr('width', pieWidth)
+    .attr('height', pieWidth)
+    .append('g')
+    .attr('transform', `translate(${pieWidth/4}, ${pieHeight/2})`);
+
+  const arc = d3.arc()
+    .outerRadius(radius/2)
+    .innerRadius(radius/4);
+
+  const pie = d3.pie()
+    .sort(null)
+    .value(d => { return d.value; });
+
+  let healthData = {
+    Yes: 0,
+    No: 0,
+    Unclear: 0,
+    Unknown: 0
+  };
+  if (groupYears.includes(year)) {
+    for (let i = 0; i < allYears.length - extraButtons; i++) {
+      const currentYear = allYears[i];
+      if (checkDecade(year, currentYear)) {
+        dataByYear[currentYear].forEach(d => {
+          healthData[d.Mental_Health_Issues]++;
+        });
+      }
+    }
+  } else {
+    dataByYear[year].forEach(d => {
+      healthData[d.Mental_Health_Issues]++;
+    });
+  }
+
+  let healthArray = [];
+  for (let prop in healthData) {
+    let color;
+    switch(prop) {
+      case 'Yes': color = rgb(200, 0, 0); break;
+      case 'No': color = rgb(46, 92, 155); break;
+      case 'Unclear': color = rgb(198, 104, 21); break;
+      default: color = rgb(43, 165, 63);
+    }
+    healthArray.push({
+      name: prop,
+      color,
+      value: healthData[prop]
+    });
+  }
+
+  createLegend(healthArray, pieDiv);
+
+  let i = healthArray.length;
+  while (i--) {
+    if (healthArray[i].value === 0) {
+      healthArray.splice(i, 1);
+    }
+  }
+
+  let dy = -4;
+
+  let g = pieSVG.selectAll('.arc')
+      .data(pie(healthArray))
+      .enter()
+      .append('g')
+      .attr('class', 'arc');
+
+  g.append('path')
+    .attr('d', arc)
+    .style('fill', d => {
+      return d.data.color;
+    });
+
+  // Center text
+  g.append('text')
+    .attr('dy', d => {
+      dy += 2;
+      return dy + 'em';
+    })
+    .text(d => {
+      return `${d.data.name}: ${d.data.value}`;
+    });
+
+  // Ring text
+  g.append('text')
+    .attr('transform', d => {
+      return `translate(${arc.centroid(d)})`;
+    })
+    .attr('dy', '0.35em')
+    .text(d => {
+      return `${d.data.name}`;
+    });
+}
+
+function createLegend(healthArray, pieDiv) {
+  let legend = pieDiv.append('svg')
+    .attr('class', 'pieLegend')
+    .style('opacity', 1)
+    // .style('left', '50px')
+    .style('top', '390px');
+
+  let boxSize = 25;
+  let yBox = boxSize;
+
+  healthArray.forEach(d => {
+    legend.append('rect')
+    .attr('x', 25)
+    .attr('y', yBox)
+    .attr('width', boxSize)
+    .attr('height', boxSize)
+    .attr('stroke', 'black')
+    .attr('stroke-width', 0.5)
+    .style('fill', d.color);
+
+    legend.append('text')
+      .attr('x', 60)
+      .attr('y', () => {
+        return yBox + boxSize * 0.75;
+      })
+      .text(d.name);
+
+    yBox += 2 * boxSize;
+  });
+
+  pieDiv.append('h4')
+    .html(`Legend`)
+    .style('position', 'absolute')
+    .style('top', '375px')
+    .style('left', '95px');
+}
+
+function loadYear(year, allYears) {
+  const maxYear = Math.max(...allYears);
+
   d3.selectAll('svg').remove();
   d3.selectAll('div').remove();
+
   d3.select('body').append('div').selectAll('button')
     .data(allYears)
     .enter()
@@ -240,28 +415,48 @@ function loadYear(year, allYears, maxYear) {
     })
     .style('margin', '0.2%')
     .on('click', d => {
-      loadYear(d, allYears, maxYear);
+      loadYear(d, allYears);
     })
     .text(d => {
-      return (d === -1) ? 'All Years' : d;
+      return getYearText(d);
     });
   createMap(year, allYears);
 }
 
+function getYearText(year) {
+  switch(year) {
+    case -1: return 'All Years';
+    case -2: return '1960s';
+    case -3: return '1970s';
+    case -4: return '1980s';
+    case -5: return '1990s';
+    case -6: return '2000s';
+    case -7: return '2010s';
+    default: return year;
+  }
+}
+
 function fixData(data) {
   data.forEach(d => {
-    d.Total_Victims = +d.Total_Victims;
     d.Fatalities = +d.Fatalities;
     d.Policeman_Killed = +d.Policeman_Killed;
     d.Injured = +d.Injured;
+    d.Total_Victims = d.Fatalities + d.Policeman_Killed + d.Injured;
     if (d.Gender === 'Male') {
       d.Gender = 'M';
+    } else if (d.Gender == 'Female') {
+      d.Gender = 'F';
     }
     if (d.Mental_Health_Issues === 'unknown') {
       d.Mental_Health_Issues = 'Unknown';
     }
     if (!d.Age) {
       d.Age = 'Unknown';
+    }
+    if (!d.Employed) {
+      d.Employed = 'Unknown';
+    } else {
+      d.Employed = (d.Employed === 1) ? 'Yes' : 'No';
     }
   });
 }
@@ -270,8 +465,7 @@ function loadMap() {
   d3.csv('./data/all-shootings.csv', data => {
     fixData(data);
     const allYears = getAllYears(data);
-    const maxYear = Math.max(...allYears);
-    loadYear(maxYear, allYears, maxYear);
+    loadYear(Math.max(...allYears), allYears);
   });
 }
 
